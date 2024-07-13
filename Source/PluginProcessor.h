e45c7b723d5c63d9435c250bdfa32b6386880263
fan7asy <blkmhntr@gmail.com>
2024-07-12 17:30:43 -0700

    Added compressorband wrapper

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -10,6 +10,41 @@
 
 #include <JuceHeader.h>
 
+struct CompressorBand
+{
+    juce::AudioParameterFloat* attack{ nullptr };
+    juce::AudioParameterFloat* release{ nullptr };
+    juce::AudioParameterFloat* threshold{ nullptr };
+    juce::AudioParameterChoice* ratio{ nullptr };
+    juce::AudioParameterBool* bypassed{ nullptr };
+
+    void prepare(const juce::dsp::ProcessSpec& spec)
+    {
+        compressor.prepare(spec);
+    }
+
+    void updateCompressorSettings()
+    {
+        compressor.setAttack(attack->get());
+        compressor.setRelease(release->get());
+        compressor.setThreshold(threshold->get());
+        compressor.setRatio(ratio->getCurrentChoiceName().getFloatValue());
+    }
+
+    void process(juce::AudioBuffer<float>& buffer)
+    {
+        auto block = juce::dsp::AudioBlock<float>(buffer);
+        auto context = juce::dsp::ProcessContextReplacing<float>(block);
+
+        context.isBypassed = bypassed->get();
+
+        compressor.process(context);
+    }
+
+private:
+    juce::dsp::Compressor<float> compressor;
+};
+
 //==============================================================================
 /**
 */
@@ -61,13 +96,14 @@ public:
 
     APVTS apvts{ *this, nullptr, "Parameters", createParameterLayout() };
 private:
-    juce::dsp::Compressor<float> compressor;
+    //juce::dsp::Compressor<float> compressor;
 
-    juce::AudioParameterFloat* attack{ nullptr };
-    juce::AudioParameterFloat* release{ nullptr };
-    juce::AudioParameterFloat* threshold{ nullptr };
-    juce::AudioParameterChoice* ratio{ nullptr };
-    juce::AudioParameterBool* bypassed{ nullptr };
-    //==============================================================================
+    //juce::AudioParameterFloat* attack{ nullptr };
+    //juce::AudioParameterFloat* release{ nullptr };
+    //juce::AudioParameterFloat* threshold{ nullptr };
+    //juce::AudioParameterChoice* ratio{ nullptr };
+    //juce::AudioParameterBool* bypassed{ nullptr };
+    CompressorBand compressor;
+    ////==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (F7MBCompAudioProcessor)
 };
