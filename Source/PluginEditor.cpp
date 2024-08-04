1cab17a6882323865f66631664aad7c5963c7ff6
fan7asy <blkmhntr@gmail.com>
2024-08-04 04:25:23 -0700

    implemented basic comp band controls

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -265,11 +265,127 @@ juce::String RotarySliderWithLabels::getDisplayString() const
 
     return str;
 }
+
+void RotarySliderWithLabels::changeParam(juce::RangedAudioParameter* p)
+{
+    param = p;
+    repaint();
+}
+//==============================================================================
+juce::String RatioSlider::getDisplayString() const
+{
+    auto choiceParam = dynamic_cast<juce::AudioParameterChoice*>(param);
+    jassert(choiceParam != nullptr);
+
+    auto currentChoice = choiceParam->getCurrentChoiceName();
+    if (currentChoice.contains(".0"))
+        currentChoice = currentChoice.substring(0, currentChoice.indexOf("."));
+
+    currentChoice << ":1";
+
+    return currentChoice;
+}
 //==============================================================================
 Placeholder::Placeholder()
 {
     juce::Random r;
     customColor = juce::Colour(r.nextInt(255), r.nextInt(255), r.nextInt(255));
+}
+//==============================================================================
+CompressorBandControls::CompressorBandControls(juce::AudioProcessorValueTreeState& apv) :
+apvts(apv),
+attackSlider(nullptr, "ms", "ATTACK"),
+releaseSlider(nullptr, "ms", "RELEASE"),
+thresholdSlider(nullptr, "dB", "THRESH"),
+ratioSlider(nullptr, "")
+
+{
+    using namespace Params;
+    const auto& params = GetParams();
+
+    auto getParamHelper = [&params, &apv](const auto& name) -> auto&
+        {
+            return getParam(apv, params, name);
+        };
+
+    attackSlider.changeParam(&getParamHelper(Names::Attack_Mid_Band));
+    releaseSlider.changeParam(&getParamHelper(Names::Release_Mid_Band));
+    thresholdSlider.changeParam(&getParamHelper(Names::Threshold_Mid_Band));
+    ratioSlider.changeParam(&getParamHelper(Names::Ratio_Mid_Band));
+
+    addLabelPairs(attackSlider.labels, getParamHelper(Names::Attack_Mid_Band), "ms");
+    addLabelPairs(releaseSlider.labels, getParamHelper(Names::Release_Mid_Band), "ms");
+    addLabelPairs(thresholdSlider.labels, getParamHelper(Names::Threshold_Mid_Band), "dB");
+    
+    ratioSlider.labels.add({ 0.f, "1:1" });
+    auto ratioParam = dynamic_cast<juce::AudioParameterChoice*>(&getParamHelper(Names::Ratio_Mid_Band));
+    ratioSlider.labels.add({ 1.0f, juce::String(ratioParam->choices.getReference(ratioParam->choices.size() - 1).getIntValue()) + ":1"});
+
+    auto makeAttachmentHelper = [&params, &apv](auto& attachment, const auto& name, auto& slider)
+        {
+            makeAttachment(attachment, apv, params, name, slider);
+        };
+
+    makeAttachmentHelper(attackSliderAttachment, Names::Attack_Mid_Band, attackSlider);
+
+    makeAttachmentHelper(releaseSliderAttachment, Names::Release_Mid_Band, releaseSlider);
+
+    makeAttachmentHelper(thresholdSliderAttachment, Names::Threshold_Mid_Band, thresholdSlider);
+
+    makeAttachmentHelper(ratioSliderAttachment, Names::Ratio_Mid_Band, ratioSlider);
+
+    addAndMakeVisible(attackSlider);
+    addAndMakeVisible(releaseSlider);
+    addAndMakeVisible(thresholdSlider);
+    addAndMakeVisible(ratioSlider);
+}
+
+void CompressorBandControls::resized()
+{
+    auto bounds = getLocalBounds().reduced(5);
+    using namespace juce;
+
+    FlexBox flexBox;
+    flexBox.flexDirection = FlexBox::Direction::row;
+    flexBox.flexWrap = FlexBox::Wrap::noWrap;
+
+    auto spacer = FlexItem().withWidth(4);
+    auto endCap = FlexItem().withWidth(6);
+
+    flexBox.items.add(endCap);
+    flexBox.items.add(FlexItem(attackSlider).withFlex(1.f));
+    flexBox.items.add(spacer);
+    flexBox.items.add(FlexItem(releaseSlider).withFlex(1.f));
+    flexBox.items.add(spacer);
+    flexBox.items.add(FlexItem(thresholdSlider).withFlex(1.f));
+    flexBox.items.add(spacer);
+    flexBox.items.add(FlexItem(ratioSlider).withFlex(1.f));
+    flexBox.items.add(endCap);
+
+    flexBox.performLayout(bounds);
+}
+
+void drawModuleBackground(juce::Graphics &g, juce::Rectangle<int> bounds)
+{
+    using namespace juce;
+
+    g.setColour(Colours::blueviolet);
+    g.fillAll();
+
+    auto localBounds = bounds;
+
+    bounds.reduce(3, 3);
+    g.setColour(Colours::white); //WHITE RECTANGLE ON TOP OF BLUEVIOLET RECTANGLE ( CHANGE LATER )
+    g.fillRoundedRectangle(bounds.toFloat(), 3);
+
+    g.drawRect(localBounds);
+}
+
+void CompressorBandControls::paint(juce::Graphics& g)
+{
+    auto bounds = getLocalBounds();
+
+    drawModuleBackground(g, bounds);
 }
 //==============================================================================
 GlobalControls::GlobalControls(juce::AudioProcessorValueTreeState& apvts)
@@ -287,10 +403,10 @@ GlobalControls::GlobalControls(juce::AudioProcessorValueTreeState& apvts)
     auto& midHighParam = getParamHelper(Names::Mid_High_Crossover_Freq);
     auto& gainOutParam = getParamHelper(Names::Gain_Out);
 
-    inGainSlider = std::make_unique<RSWL>(gainInParam, "dB", "INPUT TRIM");
-    lowMidXoverSlider = std::make_unique<RSWL>(lowMidParam, "Hz", "LOW-MID X-OVER");
-    midHighXoverSlider = std::make_unique<RSWL>(midHighParam, "Hz", "MID-HI X-OVER");
-    outGainSlider = std::make_unique<RSWL>(gainOutParam, "dB", "OUTPUT TRIM");
+    inGainSlider = std::make_unique<RSWL>(&gainInParam, "dB", "INPUT TRIM");
+    lowMidXoverSlider = std::make_unique<RSWL>(&lowMidParam, "Hz", "LOW-MID X-OVER");
+    midHighXoverSlider = std::make_unique<RSWL>(&midHighParam, "Hz", "MID-HI X-OVER");
+    outGainSlider = std::make_unique<RSWL>(&gainOutParam, "dB", "OUTPUT TRIM");
 
     auto makeAttachmentHelper = [&params, &apvts](auto& attachment, const auto& name, auto& slider)
     {
@@ -318,19 +434,9 @@ GlobalControls::GlobalControls(juce::AudioProcessorValueTreeState& apvts)
 
 void GlobalControls::paint(juce::Graphics& g)
 {
-    using namespace juce;
-
     auto bounds = getLocalBounds();
-    g.setColour(Colours::blueviolet);
-    g.fillAll();
 
-    auto localBounds = bounds;
-
-    bounds.reduce(3, 3);
-    g.setColour(Colours::white); //WHITE RECTANGLE ON TOP OF BLUEVIOLET RECTANGLE ( CHANGE LATER )
-    g.fillRoundedRectangle(bounds.toFloat(), 3);
-
-    g.drawRect(localBounds);
+    drawModuleBackground(g, bounds);
 }
 
 void GlobalControls::resized()
@@ -363,10 +469,12 @@ F7MBCompAudioProcessorEditor::F7MBCompAudioProcessorEditor (F7MBCompAudioProcess
 {
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
+    setLookAndFeel(&lnf);
     //addAndMakeVisible(controlBar);
     //addAndMakeVisible(analyzer);
+
     addAndMakeVisible(globalControls);
-    //addAndMakeVisible(bandControls);
+    addAndMakeVisible(bandControls);
 
 
     setSize (600, 500);
@@ -374,6 +482,7 @@ F7MBCompAudioProcessorEditor::F7MBCompAudioProcessorEditor (F7MBCompAudioProcess
 
 F7MBCompAudioProcessorEditor::~F7MBCompAudioProcessorEditor()
 {
+    setLookAndFeel(nullptr);
 }
 
 //==============================================================================
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -28,20 +28,20 @@ struct LookAndFeel : juce::LookAndFeel_V4
 
 struct RotarySliderWithLabels : juce::Slider
 {
-    RotarySliderWithLabels(juce::RangedAudioParameter& rap, const juce::String& unitSuffix, const juce::String& title/* = "NO TITLE"*/) :
+    RotarySliderWithLabels(juce::RangedAudioParameter* rap, const juce::String& unitSuffix, const juce::String& title/* = "NO TITLE"*/) :
         juce::Slider(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag,
             juce::Slider::TextEntryBoxPosition::NoTextBox),
-        param(&rap),
+        param(rap),
         suffix(unitSuffix)
     {
         setName(title);
-        setLookAndFeel(&lnf);
+        //setLookAndFeel(&lnf);
     }
 
-    ~RotarySliderWithLabels()
-    {
-        setLookAndFeel(nullptr);
-    }
+    //~RotarySliderWithLabels()
+    //{
+    //    setLookAndFeel(nullptr);
+    //}
 
     struct LabelPos
     {
@@ -54,12 +54,19 @@ struct RotarySliderWithLabels : juce::Slider
     void paint(juce::Graphics& g) override;
     juce::Rectangle<int> getSliderBounds() const;
     int getTextHeight() const { return 14; }
-    juce::String getDisplayString() const;
-private:
-    LookAndFeel lnf;
+    virtual juce::String getDisplayString() const;
 
+    void changeParam(juce::RangedAudioParameter* p);
+protected:
     juce::RangedAudioParameter* param;
     juce::String suffix;
+};
+
+struct RatioSlider : RotarySliderWithLabels
+{
+    RatioSlider(juce::RangedAudioParameter* rap, const juce::String& unitSuffix) : RotarySliderWithLabels(rap, unitSuffix, "RATIO") {}
+
+    juce::String getDisplayString() const override;
 };
 
 struct PowerButton : juce::ToggleButton {};
@@ -149,6 +156,21 @@ void addLabelPairs(Labels& labels, const ParamType& param, const SuffixType& suf
     labels.add({ 1.f, getValString(param, false, suffix) });
 }
 
+struct CompressorBandControls : juce::Component
+{
+    CompressorBandControls(juce::AudioProcessorValueTreeState& apvts);
+    void paint(juce::Graphics& g) override;
+    void resized() override;
+private:
+    juce::AudioProcessorValueTreeState& apvts;
+
+    RotarySliderWithLabels attackSlider, releaseSlider, thresholdSlider /*ratioSlider*/;
+    RatioSlider ratioSlider;
+
+    using Attachment = juce::AudioProcessorValueTreeState::SliderAttachment;
+    std::unique_ptr<Attachment> attackSliderAttachment, releaseSliderAttachment, thresholdSliderAttachment, ratioSliderAttachment;
+};
+
 struct GlobalControls : juce::Component
 {
     GlobalControls(juce::AudioProcessorValueTreeState& apvts);
@@ -175,12 +197,14 @@ public:
     void resized() override;
 
 private:
+    LookAndFeel lnf;
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     F7MBCompAudioProcessor& audioProcessor;
 
-    Placeholder controlBar, analyzer, /*globalControls,*/ bandControls;
+    Placeholder controlBar, analyzer /*globalControls,*/ /*bandControls*/;
     GlobalControls globalControls { audioProcessor.apvts };
+    CompressorBandControls bandControls { audioProcessor.apvts };
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (F7MBCompAudioProcessorEditor)
 };
