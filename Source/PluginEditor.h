34e31471a40df4c53bcc21dbb7cd5debb652257e
fan7asy <blkmhntr@gmail.com>
2024-08-05 22:57:06 -0700

    added solo/mute/bypass click functionality

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -156,11 +156,14 @@ void addLabelPairs(Labels& labels, const ParamType& param, const SuffixType& suf
     labels.add({ 1.f, getValString(param, false, suffix) });
 }
 
-struct CompressorBandControls : juce::Component
+struct CompressorBandControls : juce::Component, juce::Button::Listener
 {
     CompressorBandControls(juce::AudioProcessorValueTreeState& apvts);
+    ~CompressorBandControls() override;
     void paint(juce::Graphics& g) override;
     void resized() override;
+
+    void buttonClicked(juce::Button* button) override;
 private:
     juce::AudioProcessorValueTreeState& apvts;
 
@@ -178,6 +181,8 @@ private:
     juce::Component::SafePointer<CompressorBandControls> safePtr {this};
 
     void updateAttachments();
+    void updateSliderEnablements();
+    void updateSoloMuteBypassToggleStates(juce::Button& clickedButton);
 };
 
 struct GlobalControls : juce::Component
