f93d4cc85493bc276dfb5c50659f6367ae593914
fan7asy <blkmhntr@gmail.com>
2024-08-18 14:11:33 -0700

    fixed asan issue in path producer

--- Source/GUI/PathProducer.cpp
+++ Source/GUI/PathProducer.cpp
@@ -19,9 +19,17 @@ void PathProducer::process(juce::Rectangle<float> fftBounds, double sampleRate)
         {
             auto size = tempIncomingBuffer.getNumSamples();
 
-            juce::FloatVectorOperations::copy(monoBuffer.getWritePointer(0, 0),
-                monoBuffer.getReadPointer(0, size),
-                monoBuffer.getNumSamples() - size);
+            jassert(size <= monoBuffer.getNumSamples());
+            size = juce::jmin(size, monoBuffer.getNumSamples());
+
+            auto writePointer = monoBuffer.getWritePointer(0, 0);
+            auto readPointer = monoBuffer.getReadPointer(0, size);
+
+            std::copy(readPointer, readPointer + (monoBuffer.getNumSamples() - size), writePointer);
+
+            //juce::FloatVectorOperations::copy(monoBuffer.getWritePointer(0, 0),
+            //    monoBuffer.getReadPointer(0, size),
+            //    monoBuffer.getNumSamples() - size);
 
             juce::FloatVectorOperations::copy(monoBuffer.getWritePointer(0, monoBuffer.getNumSamples() - size),
                 tempIncomingBuffer.getReadPointer(0, 0),
